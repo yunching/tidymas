2f430608783b8c4682ee24dd6cf120fd14b74c12
Yun Ching Lim <yunching.lim@gmail.com>
2018-11-12 22:30:07 -0500

    Update tolerance of market_capping's output checks

--- scripts/check_sfs.R
+++ scripts/check_sfs.R
@@ -7,16 +7,38 @@ rownames(hist_mv) <- hist_mv[,1]
 hist_mv <- hist_mv[,2:length(hist_mv)]
 hist_mv
 
-#load historical sfs
-hist_cap <- read.csv("./data2/sf_check.csv", nrows=12, skip=14)
+#load historical caps
+hist_cap <- read.csv("./data2/sf_check.csv", nrows=12, skip=30)
 rownames(hist_cap) <- hist_cap[,1]
 hist_cap <- hist_cap[,2:length(hist_cap)]
-hist_cap <- hist_cap / 100 #rescale to percentage
 hist_cap
 
+#load historical wts
+hist_wts <- read.csv("./data2/sf_check.csv", nrows=12, skip=16)
+rownames(hist_wts) <- hist_wts[,1]
+hist_wts <- hist_wts[,2:length(hist_wts)]
+hist_wts
+
 #calc sfs from historical mvs
-#here I've just used no capping constraints
-purrr::map2(hist_mv, data.frame(matrix(rep(1, 240), nrow = 12)), tidymas::market_capping)
+results <- purrr::map2(hist_mv, data.frame(hist_cap), tidymas::market_capping)
+calculated_wts <- purrr::map(results, "capped_mv_wts") #%>% purrr::map(function(x) round(x, 2))
+countries <- c(
+  "Australia",
+  "Belgium",
+  "Canada",
+  "France",
+  "Germany",
+  "Italy",
+  "Japan",
+  "Netherlands",
+  "S. Korea",
+  "Spain",
+  "United Kingdom",
+  "United States"
+)
+calculated_wts <- data.frame(calculated_wts, row.names=countries)
+diff <- round(calculated_wts - hist_wts, 3)
+diff
 
 #compare sfs with historical sfs
-#TBC
+
