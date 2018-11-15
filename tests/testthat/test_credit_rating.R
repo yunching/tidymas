1a1ec4ec25d8b1d97b459af17c5ef1694b10d20f
Yun Ching Lim <yunching.lim@gmail.com>
2018-11-15 01:19:51 -0500

    Adding credit rating functionality

---
+++ tests/testthat/test_credit_rating.R
@@ -0,0 +1,48 @@
+context("Credit rating translations")
+
+library(tidymas)
+library(stringr)
+
+test_that("AAA",{
+  expect_equal(clean_rating("AAA"), "AAA")
+  expect_equal(clean_rating("AAAu"), "AAA")
+  expect_equal(clean_rating("AAAu   *-"), "AAA")
+  expect_equal(clean_rating("Aaa"), "AAA")
+  expect_equal(clean_rating("Aaa    *-"), "AAA")
+})
+
+test_that("AA+",{
+  expect_equal(clean_rating("AA+"), "AA+")
+  expect_equal(clean_rating("AA+u"), "AA+")
+  expect_equal(clean_rating("Aa1"), "AA+")
+  expect_equal(clean_rating("Aa1    *-"), "AA+")
+})
+
+test_that("AA",{
+  expect_equal(clean_rating("AAu"), "AA")
+  expect_equal(clean_rating("AAu    *-"), "AA")
+  expect_equal(clean_rating("AA"), "AA")
+  expect_equal(clean_rating("Aa2"), "AA")
+  expect_equal(clean_rating("Aa2    *-"), "AA")
+  expect_equal(clean_rating("AA"), "AA")
+  expect_equal(clean_rating("AA     *-"), "AA")
+
+})
+
+test_that("AA-",{
+  expect_equal(clean_rating("AA-u"), "AA-")
+  expect_equal(clean_rating("AA-    *-"), "AA-")
+  expect_equal(clean_rating("Aa3"), "AA-")
+  expect_equal(clean_rating("Aa3    *-"), "AA-")
+  expect_equal(clean_rating("AA-"), "AA-")
+  expect_equal(clean_rating("AA-    *+"), "AA-")
+  expect_equal(clean_rating("AA-    *-"), "AA-")
+})
+
+test_that("A+",{
+  expect_equal(clean_rating("A+"), "A+")
+  expect_equal(clean_rating("A+u"), "A+")
+  expect_equal(clean_rating("A1"), "A+")
+  expect_equal(clean_rating("A+"), "A+")
+  expect_equal(clean_rating("A+     *-"), "A+")
+})
