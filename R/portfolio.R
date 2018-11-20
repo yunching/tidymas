b86d623b090127a8eb1e1c2f17fb0add205890e9
Yun Ching Lim <yunching.lim@gmail.com>
2018-11-19 21:08:15 -0500

    Release for z-score

--- R/portfolio.R
+++ R/portfolio.R
@@ -7,10 +7,9 @@
 #'
 #' @examples
 #' z_score(1:10)
-#' #[1] -1.4863011 -1.1560120 -0.8257228 -0.4954337 -0.1651446  0.1651446  0.4954337  0.8257228  1.1560120  1.4863011
 z_score <- function(my_ts){
   my_mean <- mean(my_ts)
-  my_sd <- sd(my_ts)
+  my_sd <- stats::sd(my_ts)
 
   return((my_ts - my_mean)/my_sd)
 }
