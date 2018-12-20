d9352ec35b94149a4da4296411169ef930f9a8f5
chowchowsheep <39787818+chowchowsheep@users.noreply.github.com>
2018-12-20 16:35:54 +0800

    Update eqd_rebal.R

--- R/eqd_rebal.R
+++ R/eqd_rebal.R
@@ -5,19 +5,19 @@ NULL
 #' Asset class rebalancing
 #'
 #' @param port.SGD Size of portfolio in Singapore dollars
-#' @param weight percentage allocation to asset class
+#' @param weight Percentage allocation to asset class
 #' @param units Existing units of asset class
+#' @param index Index close - can download from bbg
 #'
 #' @return Number of units to add to portfolio (-ve means reduce allocation)
 #' @export
 #'
 #' @examples
 #' \donttest{
-#' eqd(1000000, 0.05, 10)
+#' eqd(1000000, 0.05, 10,1452.38)
 #' }
-eqd <- function(port.SGD, weight, units) {
+eqd <- function(port.SGD, weight, units, index) {
   SGD <- bdp("SGD WMCD Curncy","PX_LAST")
-  index <- bdp("M1CXDBKB Index","PX_LAST")
   R2.USD <- port.SGD/SGD[1,1]
   eqd.not <- units * index[1,1]
   weight <- ifelse(weight > 1, weight/100, weight)
